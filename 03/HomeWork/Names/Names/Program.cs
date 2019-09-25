9632b5bb614b4dab55060e9e7410caa013296e5c
ledzs <leksakov01@ya.ru>
2019-09-26 01:29:49 +0300

    HomeWork 03

---
+++ 03/HomeWork/Names/Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = new string[3];
+            byte[] ages = new byte[3];
+            for (int i = 0; i <= 2; i++ )
+            {
+                Console.WriteLine($"Enter name {i+1}");
+                names[i] = Console.ReadLine();
+                Console.WriteLine($"Enter {names[i]}'s age");
+                ages[i] = byte.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine($"Name: {names[i]}, age in 4 years: {ages[i]+4}");
+                
+            }
+            Console.WriteLine("Press any key to complete");
+            Console.ReadKey();
+        }
+    }
+}
