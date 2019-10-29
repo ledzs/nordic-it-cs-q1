4677747096c95c3b1788ab6d80cce4465c364e6b
ledzs <leksakov01@ya.ru>
2019-10-29 03:05:33 +0300

    HomeWork 07

---
+++ 07/HomeWork/Words begining A/Words begining A/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Words_begining_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите строку минимум из двух слов (на русском языке)");
+                    string words = Console.ReadLine();
+                    string[] wordsArray = words.Split(' '); //разбиваем введенную строку на массив с разделителем пробел
+                    int k = 0; //для счетчика количества слов
+                    if (wordsArray.Length >= 2)
+                    {
+                        for (int i = 0; i < wordsArray.Length; i++)
+                        {
+                            wordsArray[i] = wordsArray[i].ToLower(); //приводим к нижнему регистру слово, чтобы сравнивать только маленькую букву "а"
+                            if (wordsArray[i].StartsWith("а"))
+                            {
+                                k++;
+                            }
+                        }
+                        Console.WriteLine($"Количество слов, начинающихся с буквы 'А': {k}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите минимум 2 слова!");
+                        continue;
+                    }
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine("Возникло исключение:");
+                    Console.WriteLine($"{e.GetType()}: {e.Message}"); 
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
