6c1dcc32fb9047f47554b8291e3e36990d57e0d2
ledzs <leksakov01@ya.ru>
2019-10-30 09:01:16 +0300

    HomeWork 10

---
+++ 10/HomeWork/HomeWork10/HomeWork10/Peoples.cs
@@ -0,0 +1,23 @@
+﻿namespace HomeWork10
+{
+    public class Peoples
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int AgeAfter4Year
+        {
+            get
+            {
+                return Age + 4;
+            }
+        }
+        public string Result
+        {
+            get
+            {
+                return $"Name: {Name}, Age in 4 years: {AgeAfter4Year}.";
+            }
+        }
+
+    }
+}
---
+++ 10/HomeWork/HomeWork10/HomeWork10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[3];
+            int[] ages = new int[3];
+            //string[] peoples = new string[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Введите имя {i+1}-го человека:");
+                names[i] = Console.ReadLine();
+                Console.WriteLine($"Введите его возраст:");
+                ages[i] = int.Parse(Console.ReadLine());
+                string p = "peoples{i}";
+
+                Peoples p = new Peoples
+                {
+                    Name = names[i],
+                    Age = ages[i]
+                };
+            }
+
+            Peoples p1 = new Peoples
+            {
+                Name = "Ivan",
+                Age = 23
+            };
+
+            Console.WriteLine(p1.Result);
+            Console.ReadKey();
+        }
+    }
+}
