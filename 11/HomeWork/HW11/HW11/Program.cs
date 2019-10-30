be6715bd9145703fd85ca2e91262ce6097c5dfbb
ledzs <leksakov01@ya.ru>
2019-10-31 01:02:07 +0300

    HomeWork 11

---
+++ 11/HomeWork/HW11/HW11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alarm1 = new ReminderItem()
+            {
+                AlarmDate = DateTimeOffset.Parse("2019-10-31 12:3:3"),
+                AlarmMessage = "Wake UP!"
+            };
+            alarm1.WriteProperties();
+
+            var alarm2 = new ReminderItem()
+            {
+                AlarmDate = DateTimeOffset.Parse("2019-10-15 23:15:10"),
+                AlarmMessage = "Wake UP!"
+            };
+            alarm2.WriteProperties();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11/HomeWork/HW11/HW11/ReminderItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace HW11
+{
+    class ReminderItem
+    {
+        DateTimeOffset alarmDate;
+        string alarmMessage;
+        public DateTimeOffset AlarmDate { get; set; }
+        public string AlarmMessage { get; set; }
+        public TimeSpan TimeToAlarm
+        {
+            get { return DateTime.Now - AlarmDate;}
+        }
+
+        public bool IsOutdated
+        {
+            get
+            {
+                if(TimeToAlarm.TotalSeconds >= 0)
+                {
+                    return true;
+                }
+             return false;
+            }
+        }
+        public ReminderItem()
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+        public void WriteProperties()
+        {
+            Console.WriteLine(
+                $"AlarmDate: {AlarmDate},\n" +
+                $"AlarmMessage: {AlarmMessage},\n" +
+                $"TimeToAlarm: {TimeToAlarm},\n" +
+                $"IsOutdated: {IsOutdated}\n");
+        }
+    }
+}
