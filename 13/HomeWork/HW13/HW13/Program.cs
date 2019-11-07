f6eeea727b3d41d751c7e742fd152ab123080318
ledzs <leksakov01@ya.ru>
2019-11-07 04:09:23 +0300

    HomeWork 13

---
+++ 13/HomeWork/HW13/HW13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace HW13
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var logInFile = new FileLogWriter();
+            var logInConsole = new ConsoleLogWriter();
+
+            var multipleLogWriter = new MultipleLogWriter(logInFile, logInConsole);
+
+            multipleLogWriter.LogError("ErrorMessage");
+            multipleLogWriter.LogInfo("InfoMessage");
+            multipleLogWriter.LogWarning("WarningMessage");
+
+            Console.ReadKey();
+        }
+    }
+}
