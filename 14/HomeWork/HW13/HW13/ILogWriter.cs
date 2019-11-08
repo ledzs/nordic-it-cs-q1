9c60facb1cbbb1a6622147f3590e84385f930ad0
ledzs <leksakov01@ya.ru>
2019-11-08 03:23:58 +0300

    HomeWork 14

---
+++ 14/HomeWork/HW13/HW13/ILogWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW13
+{
+    public interface ILogWritter
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+    }
+}
---
+++ 14/HomeWork/HW13/HW13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace HW13
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            //****расположение лога меняется в классе FileLogWriter.cs*****
+
+            var logInFile = FileLogWriter.GetInstance();
+            var logInConsole = ConsoleLogWriter.GetInstance();
+
+
+            var multipleLogWriter = new MultipleLogWriter(logInFile, logInConsole);
+
+            multipleLogWriter.LogError("ErrorMessage");
+            multipleLogWriter.LogInfo("InfoMessage");
+            multipleLogWriter.LogWarning("WarningMessage");
+
+            Console.ReadKey();
+        }
+    }
+}
